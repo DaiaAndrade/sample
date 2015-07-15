227692c0c9c2f18bb24b80e77abfabf701dcf9e8
DaiaAndrade <daiafocking@gmail.com>
2015-07-15 02:44:31 -0400

    commented code

--- js/app/scripts/main.js
+++ js/app/scripts/main.js
@@ -1,18 +1,55 @@
+/*
+	main.js
+	Daiane Andrade
+	July 2015
+
+	This code implements a sample of a simulation
+	for integration with SmartSparrow
+
+	This code has the controller, queue and funtions
+	for integrate with HTML
+
+*/
+
+//	Controller for the model
 function ViewController(){
+	//	Create the shape model
 	this.shapeModel = new ShapeModel();
+	//	Create the view model with the shape of the Shape Model
 	this.shapeView = new ViewModel(this.shapeModel.getShape());
 }
 
+//	Function for draw the shape in the canvas
 ViewController.prototype.drawCanvas = function(){
+	//	Tell de view to drap the shape
 	this.shapeView.drawShape();
 }
 
+ViewController.prototype.resetController = function(){
+	this.shapeModel.shapeReset();
+	this.shapeView.resetView(this.shapeModel.getShape());
+}
+/*	
+	Queue data structure.
+	A queue is a vector with no information inside of it
+	It is used to control the Answers based in the size of the queue that
+	you choose
+*/
+
 function Queue(size){
+	//	Create the Queue as a vector
 	this.queue = [];
 	for (var i = 0; i < size ; i++){
+		//	Put null informaton inside of it
 		this.queue.push(null);
 	}
 }
+
+/*
+	function to check if the queue has the correct
+	amount of true inside of it. Used to 
+	finish the questions
+*/
 
 Queue.prototype.queueCheck = function(){
 	var trueCounter = this.trueCounter();
@@ -24,11 +61,20 @@ Queue.prototype.queueCheck = function(){
 	}
 }
 
+/*	
+	function to add the value at the end of the queue
+	and remove the first element of the queue	
+*/
+
 Queue.prototype.queueAdd = function(value){
 	this.queue.splice(0,1);
 	this.queue.push(value);
 }
 
+/*	
+	function that counts the amount of true values
+	inside the queue
+*/
 Queue.prototype.trueCounter = function(){
 	var trueCounter = 0;
 	for (var count in this.queue){
@@ -39,6 +85,10 @@ Queue.prototype.trueCounter = function(){
 	return trueCounter;
 }
 
+/*	
+	serie of code to create and expose variables
+	to SmartSparrow	
+*/
 var simModel = new pipit.CapiAdapter.CapiModel({
 	done: false
 });
@@ -46,46 +96,74 @@ var simModel = new pipit.CapiAdapter.CapiModel({
 pipit.CapiAdapter.expose('done', simModel);
 pipit.Controller.notifyOnReady();
 
+/*	Global variables to be used during the execution	*/
 var viewController = new ViewController();
 var queue = new Queue(5);
 
+/*	main function where the graph should be drawn	*/
 main = function(){
 	viewController.drawCanvas();
 }
 
+//	Function for the question
 questions = function() {
-	var answer1 = $('input[name="optionsRadios"]:checked').val();;
+	//	Take the element from the HTML
+	//	There's different ways to make this, but for radio structure
+	//	this one was the most effective one, using jquery
+	var answer1 = $('input[name="optionsRadios"]:checked').val();
+
+	//	Is the answer from the HTML equal to the actual shape?
 	if(answer1 == viewController.shapeModel.getShape()){
+		//	add true in the queue
 		queue.queueAdd(true);
 	}
 	else{
+		//	add false in the queue
 		queue.queueAdd(false);
 	}
+
+	//	viewers tracking of answers
 	document.getElementById('correct1').innerHTML = "Number of right Answers: " + queue.trueCounter();
+
+	//	Do we have the right queue answers?
 	if (queue.queueCheck() == true) {
 		document.getElementById('correct1').innerHTML = "You got it right!";
+		/*	change the status of the SmartSparrow variable for true to show that
+			the student is able to continue the lesson	*/
 		simModel.set('done', true);
+		//	expose the answer for the SmartSparrow
 		pipit.CapiAdapter.expose('done', simModel);
 		pipit.Controller.notifyOnReady();
 	}
-
+	// The amount is not right
 	else{
-		viewController = new ViewController();
+		//	Reset the Controller
+		viewController.resetController();
+		//	Draw in the canvas
 		main();
 
 	}
 }
 
+//	Send button for the questions
 $('#send1').click(function(){
+	//	Do we have the right queue answers?
 	if (queue.queueCheck() == true) {
+		//	Hide the question
 		$("#form1").css({"display": "none"});
 	}
 	else{
-		viewController = new ViewController();
+		//	Reset the Controller
+		viewController.resetController();
+		//	Draw in the canvas
 		main();
 	}
 })
 
+/*	when you load the page, in the CSS,
+	the question is hidden, and you just want to show
+	it when the page is fully loaded,
+	so we made this function	*/
 var show = function(){
 	$("#form1").css({"display": "block"});
 }
--- js/app/scripts/model.js
+++ js/app/scripts/model.js
@@ -1,9 +1,27 @@
+/*
+	model.js
+	Daiane Andrade
+	July 2015
+
+	This code implements a sample of ShapeModel
+	and ShapeView and how it works
+
+	This code has the models of the shape and the view
+
+*/
+
 function ShapeModel(){
 	this.shapeChoose();
 }
 
+//	Function to choose the shape for the canvas
 ShapeModel.prototype.shapeChoose = function(){
+	//	Take a random number
 	var randNum = Math.random();
+	/*
+		Since we have 2 types of shape, below 0.5 is one of them
+		and above is the other one
+	*/
 	if (randNum > 0.5) {
 		this.shapeName = "rect";
 	}
@@ -12,60 +30,83 @@ ShapeModel.prototype.shapeChoose = function(){
 	}
 }
 
+//	Function to reset the shape
 ShapeModel.prototype.shapeReset = function(){
 	this.shapeName = null;
 	this.shapeChoose();
 }
 
+//	Returns the shape name
 ShapeModel.prototype.getShape = function(){
 	return this.shapeName;
 }
 
-
+/*
+	Model for create the view.
+	Recieve the name from the Shape Model and set
+	It from there
+*/
 function ViewModel(shapeName){
 	this.setupShapeView(shapeName);
 }
 
+/*
+	Create the Shapeview. The shape if drawn on a canvas
+	The shape given by the ShapeModel defines the shape
+	that will be drawn in the canvas
+*/
 ViewModel.prototype.setupShapeView = function(shapeName){
+	//	Handle for the canvas
 	this.shapeCanvas = document.getElementById('shapeCanvas');	
+	//	Handle or the canvas context
 	this.shapeContext = this.shapeCanvas.getContext('2d');
+	//	Erase the canvas
 	this.shapeContext.clearRect(0, 0, this.shapeCanvas.width, this.shapeCanvas.height);
+	//	Set the canvas width
 	this.shapeContext.canvas.width  = 400;
+	//	Set the name of the shape
 	this.shapeName = shapeName;
 }
 
+//	Function to reset the shape
+ViewModel.prototype.resetView = function(shapeName){
+	this.shapeName = shapeName;
+}
+
+//	Function to select the shape that wil be drawn
 ViewModel.prototype.drawShape = function(){
+	//	Erase the canvas
 	this.shapeContext.clearRect(0, 0, this.shapeCanvas.width, this.shapeCanvas.height);
+	//	Set the size
 	this.shapeContext.canvas.width  = 400;
 	this.shapeContext.canvas.height  = 225;
+	//	Which shape should be drawn?
 	if (this.shapeName == "rect") {
+		//	A rectangle
 		this.drawRect();
 	}
 	else{
+		//	A circle
 		this.drawCircle();
 	}
 }
 
+//	Function to draw rectangle
 ViewModel.prototype.drawRect = function(){
+	//	Start the drawing path
 	this.shapeContext.beginPath();
+	//	Set the fill for the rectangle
 	this.shapeContext.fillStyle = "black";
+	//	fill the rectangle with (x,y, height, width)
 	this.shapeContext.fillRect(100,25,200,200);
 }
 
+//	Function to draw circle
 ViewModel.prototype.drawCircle = function(){
+	//	Start the drawing path
 	this.shapeContext.beginPath();
+	//	Create a path for the circle with (x,y,radius, start angle, end angle)
 	this.shapeContext.arc(200,115,100,0,2*Math.PI);
+	//	Create the arc
 	this.shapeContext.stroke();
-}
-
-ViewModel.prototype.clearCanvas = function(){
-	if (this.shapeName == "rect") {
-		this.shapeContext.clearRect(100,25,200,200);
-	}
-	else{
-		this.shapeContext.strokeStyle = "white";
-		this.shapeContext.beginPath();
-		this.shapeContext.arc(200,115,100,0,2*Math.PI);
-		this.shapeContext.stroke();
-	}
 }
